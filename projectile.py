860c9f55b400ff4b9bad7704f2bcc4c6deb57d6f
Patrick Muldoon <doon@labratsoftware.com>
2018-08-25 21:51:24 -0700

    simple changes to test writing to a ppm file

--- projectile.py
+++ projectile.py
@@ -20,14 +20,23 @@ def tick(world: World, p: Projectile) -> Projectile:
 
 
 start = rt.Point(0, 1, 0)
-vel = rt.Vector(1, 1, 0).normalize() * 4
+vel = rt.Vector(1, 1.8, 0).normalize() * 11.25
 p = Projectile(start, vel)
-w = World(rt.Vector(0, -0.1, 0), rt.Vector(-0.01, 0, 0))
-
+gravity = rt.Vector(0, -0.1, 0)
+wind = rt.Vector(-0.01, 0, 0)
+w = World(gravity, wind)
+c = rt.Canvas(900, 550)
+c1 = rt.Color(1, 0, 0)
 t = 0
+
 while p.position.y > 0:
     print(f"Tick {t}: Position {p.position}")
+
+    c.write_pixel(int(p.position.x), int(550 - p.position.y), c1)
     p = tick(w, p)
     t = t + 1
 
 print(f"\nTook {t} ticks to hit ground")
+
+with open("foo.ppm", "w") as ppm_file:
+    ppm_file.write(c.to_ppm())
